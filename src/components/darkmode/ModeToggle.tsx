c21a8fb50c62e22ef58fef81af22d35005e3f6fd
Faroeq <alves_1044@hotmail.com>
2024-08-09 00:42:19 +0200

    Style darkmode toggle according to design

--- src/components/darkmode/ModeToggle.tsx
+++ src/components/darkmode/ModeToggle.tsx
@@ -1,5 +1,3 @@
-import { Moon, Sun } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -8,6 +6,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useTheme } from "./ThemeProvider";
+import MoonIcon from "../icons/MoonIcon";
+import SunIcon from "../icons/SunIcon";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
@@ -15,9 +15,13 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
+          variant="outline"
+          size="icon"
+          className="bg-transparent border-0 dark:bg-transparent dark:border-0"
+        >
+          <SunIcon className="transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100 fill-white" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
