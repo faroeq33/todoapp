ec5f0999e0403a96256e855eb50eebcdc7dc2cfd
Faroeq <alves_1044@hotmail.com>
2024-08-09 16:06:56 +0200

    Fix background image for darkmode

--- src/components/custom-ui/BackGroundImage.tsx
+++ src/components/custom-ui/BackGroundImage.tsx
@@ -1,11 +1,16 @@
+import useTheme from "../darkmode/useTheme";
+
 function BackGroundImage() {
-  return (
-    <img
-      src="src/assets/images/bg-mobile-light.jpg"
-      alt="Chanel Perfume"
-      className="w-full"
-    />
-  );
+  const { theme } = useTheme();
+
+  const getThemeImage = () => {
+    if (theme === "light") {
+      return "src/assets/images/bg-mobile-light.jpg";
+    }
+    return "src/assets/images/bg-mobile-dark.jpg";
+  };
+
+  return <img src={getThemeImage()} alt="Chanel Perfume" className="w-full" />;
 }
 
 export default BackGroundImage;
