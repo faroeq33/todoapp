5c4a268cfa569cc4859dc5d91c0bb63729f27990
Faroeq <alves_1044@hotmail.com>
2024-08-13 13:28:28 +0200

    Rename logo in nav

--- src/components/custom-ui/Nav.tsx
+++ src/components/custom-ui/Nav.tsx
@@ -1,11 +1,11 @@
-import Logo from "./Logo";
 import { ModeToggle } from "../darkmode/ModeToggle";
+import CustomLogo from "./CustomLogo";
 
 function Nav() {
   return (
     <nav className="bg-transparent">
       <div className="flex flex-row justify-between px-4">
-        <Logo />
+        <CustomLogo />
         <ModeToggle />
       </div>
     </nav>
